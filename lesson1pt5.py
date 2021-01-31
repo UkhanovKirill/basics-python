74390b80bdd71cc1010f903b2e9491d595edfa76
UkhanovKirill <ukhanovkirill1@gmail.com>
2021-01-31 17:09:51 +0300

    all practical task

---
+++ lesson1pt5.py
@@ -0,0 +1,10 @@
+profit = int(input("Введите выручку фирмы "))
+costs = int(input("Введите издержки фирмы "))
+if profit > costs:
+    print(f"Фирма работает с прибылью. Рентабельность выручки составила {profit / costs:.2f}")
+    workers = int(input("Введите количество сотрудников фирмы "))
+    print(f"прибыль в расчете на одного сторудника сотавила {profit / workers:.2f}")
+elif profit == costs:
+    print("Фирма работает в ноль")
+else:
+    print("Фирма работает в убыток")
