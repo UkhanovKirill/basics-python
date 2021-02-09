7da0ab11ef57c022da96d39c01bd7fcd229d3e04
UkhanovKirill <ukhanovkirill1@gmail.com>
2021-02-09 16:13:55 +0300

    practical task lesson 3

--- lesson3pt2.py
+++
@@ -1,9 +0,0 @@
-name = input('Введите ваше имя: ')
-surname = input('Введите вашу фамилию: ')
-year = int(input('Введите ваш год рождения: '))
-city = input('Введите город проживания: ')
-email = input('Введите вашу почту: ')
-telephone = input('Введите ваш телефон: ')
-def my_func(**kwargs):
-    return print(kwargs)
-my_func(name = name, surname = surname, year = year, city = city, email = email, telephone = telephone)
